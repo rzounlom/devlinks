136f149ef1bfe3807545753684e4049fe3d81926
Romaric Zounlome <rzounlom@gmail.com>
2024-02-27 08:03:18 -0500

    chore :remove sign-up page

--- src/components/auth/auth-form.tsx
+++ src/components/auth/auth-form.tsx
@@ -38,98 +38,6 @@ const AuthForm: FC<AuthFormProps> = ({ authType }) => {
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
         <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
-          <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Password
-              </label>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                />
-                <label
-                  htmlFor="remember-me"
-                  className="ml-3 block text-sm leading-6 text-gray-900"
-                >
-                  Remember me
-                </label>
-              </div>
-
-              <div className="text-sm leading-6">
-                {authType === "sign-in" ? (
-                  <>
-                    {"Don't have an account? "}
-                    <Link
-                      href="/sign-up"
-                      className="font-semibold text-indigo-600 hover:text-indigo-500"
-                    >
-                      Sign up
-                    </Link>
-                  </>
-                ) : (
-                  <>
-                    {"Already have an account? "}
-                    <Link
-                      href="/sign-in"
-                      className="font-semibold text-indigo-600 hover:text-indigo-500"
-                    >
-                      Sign in
-                    </Link>
-                  </>
-                )}
-              </div>
-            </div>
-
-            <div>
-              <FormButton
-                styles={
-                  "flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                }
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                {authType === "sign-in" ? "Sign in" : "Sign up"}
-              </FormButton>
-            </div>
-          </form>
-
           <div>
             <div className="relative mt-10">
               <div
@@ -140,7 +48,7 @@ const AuthForm: FC<AuthFormProps> = ({ authType }) => {
               </div>
               <div className="relative flex justify-center text-sm font-medium leading-6">
                 <span className="bg-white px-6 text-gray-900">
-                  Or continue with
+                  Continue with
                 </span>
               </div>
             </div>
